81b1d9c1d3bfbce2064c4e83e5725f784ec2fd7d
Artur <arturvl1@hotmail.com>
2022-09-01 18:13:23 -0300

    requisito 5 em andamento

--- script.js
+++ script.js
@@ -1,16 +1,28 @@
-function colorChange (){
+function colorChange() {
   const blocoDeCores = document.querySelectorAll('.color');
-  for(let i = 0; i < blocoDeCores.length ; i += 1){
-    if(blocoDeCores[i] == blocoDeCores[0]){
-      blocoDeCores[i].style.backgroundColor = 'black';
+  for (let i = 0; i < blocoDeCores.length; i += 1) {
+    if (blocoDeCores[i] === blocoDeCores[0]) {
+      blocoDeCores[i].style.backgroundColor = 'rgb(0,0,0)';
     } else {
-      let r = Math.floor(Math.random()*255);
-      let g = Math.floor(Math.random()*255);
-      let b = Math.floor(Math.random()*255);
-    blocoDeCores[i].style.backgroundColor = 'rgb(' + r + ',' + g + ',' + b +')';
+      const r = Math.floor(Math.random() * 255);
+      const g = Math.floor(Math.random() * 255);
+      const b = Math.floor(Math.random() * 255);
+      blocoDeCores[i].style.backgroundColor = 'rgb('+r+','+g+','+b+')';
     }
   }
-};
+}
 
-let button = document.querySelector('#button-random-color');
+const button = document.querySelector('#button-random-color');
 button.addEventListener('click', colorChange);
+
+function armazenaCores() {
+  const blocoDeCores = document.querySelectorAll('.color');
+  let f = JSON.parse(localStorage.getItem('colorPalette')) || [];
+  for (let i = 0; i < blocoDeCores.length; i += 1) {
+    const r = JSON.stringify(blocoDeCores[i].style.backgroundColor);
+    f.push(r);
+  }
+  localStorage.setItem('colorPalette', f);
+}
+
+document.querySelector('#button-random-color').onclick = armazenaCores;
