91b183e5e5ae54296ea23b8995aa36795009022d
Artur <arturvl1@hotmail.com>
2022-09-01 14:34:08 -0300

    requisito 4

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
-function colorChange (color){
-    const blocoDeCores = document.getElementsByClassName('color');
+function colorChange (){
+  const blocoDeCores = document.querySelectorAll('.color');
   for(let i = 0; i < blocoDeCores.length ; i += 1){
-    blocoDeCores[i].style.backgroundColor = 'color'
+    if(blocoDeCores[i] == blocoDeCores[0]){
+      blocoDeCores[i].style.backgroundColor = 'black';
+    } else {
+      let r = Math.floor(Math.random()*255);
+      let g = Math.floor(Math.random()*255);
+      let b = Math.floor(Math.random()*255);
+    blocoDeCores[i].style.backgroundColor = 'rgb(' + r + ',' + g + ',' + b +')';
+    }
   }
-}
-colorChange('black');
+};
+
+let button = document.querySelector('#button-random-color');
+button.addEventListener('click', colorChange);
